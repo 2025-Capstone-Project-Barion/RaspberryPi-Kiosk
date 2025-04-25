14e2e825f2c61912999affcbfd003ec92cc7d764
wjlee <000jun2626@gmail.com>
2025-04-26 05:37:56 +0900

    [FEAT/#19] Electron 제거 및 Picovoice Porcupine학습 [English: "Hey Barry on"]

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import FrontPage from '../pages/FrontPage';
 import MenuPage from '../pages/MenuPage';
 
 const Router = () => {
     return (
-        <HashRouter>
+        <BrowserRouter>
             <Routes>
                 <Route path="/" element={<FrontPage />} />
                 <Route path="/MenuPage" element={<MenuPage />} />
                 {/* <Route path="/payment" component={Payment} /> */}
             </Routes>
-        </HashRouter>
+        </BrowserRouter>
     );
 }
 
